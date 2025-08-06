c8b915c90bf65c9b5b65d9e6a3faf77e33e7b729
radwa.ismail <radwaismail444@gmail.com>
2025-08-06 11:03:10 +0300

    new files

--- demo/src/main/java/com/example/demo/entity/User.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-public class User {
-
-}
---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, LocalDateTime createdAt) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+    }}
